99cb124248c919d78ed027e1d41f8618fbd80ba5
Jonah Ifeoma <ifyajoke@gmail.com>
2020-05-17 00:34:29 +0100

    Feat:Added flags

--- client/js/app.js
+++ client/js/app.js
@@ -1,9 +1,11 @@
+//REST API URL
 const baseURL = "https://restcountries.eu/rest/v2/all";
 
 const dropDown = document.querySelector('.form__dropdown--content');
 const selectRegion = document.querySelector('.form__dropdown--btn');
 const darkMode = document.querySelector('.navbar__mode--link');
 const body = document.querySelector('body');
+const countrySection = document.querySelector('.country__section');
 
 
 
@@ -17,7 +19,7 @@ function showDropdown(e){
     //rotate span
     document.querySelector('.dropdown__icon').classList.toggle('rotate');
    
-   
+   console.log(e.target)
 }
 
 
@@ -33,12 +35,57 @@ function switchMode(e){
     console.log('clicked');
 
     }
+
+    
 function countries(){
+    //get data from api
     getData(baseURL)
+    //manipulate DOM with Data
     .then(function(data){
         for (i = 0; i < data.length; i++) {
-            console.log(i);
+            const countryDiv = document.createElement('div');
+            countryDiv.className = 'country__box';
+            countrySection.appendChild(countryDiv);
+            //create image or flag and append
+            const flag = document.createElement('img');
+            flag.src = data[i].flag;
+            flag.alt = 'flag';
+            flag.style.width = '100%';
+            flag.style.height = '50%';
+            flag.style.objectFit = 'cover';
+            countryDiv.appendChild(flag);
+            //create name
+            const name = document.createElement('h2');
+            name.className = 'country__heading';
+            name.textContent = data[i].name;
+            countryDiv.appendChild(name);
+            //create paragraph details
+            //population
+            const population = document.createElement('p');
+            population.className = 'country__paragraph'
+            pop = `Population: <span> ${data[i].population} </span>`;
+            population.innerHTML = pop;
+            countryDiv.appendChild(population);
+            //capital
+            const capital = document.createElement('p');
+            capital.className = 'country__paragraph'
+            pop = `Capital: <span> ${data[i].capital} </span>`;
+            capital.innerHTML = pop;
+            countryDiv.appendChild(capital);
+            //callingcodes
+            const callingCodes = document.createElement('p');
+            callingCodes.className = 'country__paragraph'
+            pop = `Calling Codes: <span> ${data[i].callingCodes} </span>`;
+            callingCodes.innerHTML = pop;
+            countryDiv.appendChild(callingCodes);
+            //Region
+            const region = document.createElement('p');
+            region.className = 'country__paragraph'
+            pop = `Region: <span> ${data[i].region} </span>`;
+            region.innerHTML = pop;
+            countryDiv.appendChild(region);
           }
+         
     })
 }
 countries();
