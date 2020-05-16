276ce709c9fd72b94a7be9889381512ecf8e5fdc
Jonah Ifeoma <ifyajoke@gmail.com>
2020-05-16 23:35:51 +0100

    Feat: setup dark theme with ja

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,45 @@
-console.log('js working');
+const baseURL = "https://restcountries.eu/rest/v2/all";
+
+const dropDown = document.querySelector('.form__dropdown--content');
+const selectRegion = document.querySelector('.form__dropdown--btn');
+const darkMode = document.querySelector('.navbar__mode--link');
+const body = document.querySelector('body');
+
+
+
+selectRegion.addEventListener('click', showDropdown);
+darkMode.addEventListener('click', switchMode);
+
+function showDropdown(e){
+    //show and hide drop down
+    dropDown.classList.toggle('hide');
+
+    //rotate span
+    document.querySelector('.dropdown__icon').classList.toggle('rotate');
+   
+   
+}
+
+
+darkMode.addEventListener('click', switchMode);
+
+function switchMode(e){
+    if(body.classList.contains('bg--dark') == false){
+        body.classList.add('bg--dark')
+    } else{
+        body.classList.remove('bg--dark')
+    }
+
+    console.log('clicked');
+
+    }
+function countries(){
+    getData(baseURL)
+    .then(function(data){
+        for (i = 0; i < data.length; i++) {
+            console.log(i);
+          }
+    })
+}
+countries();
+  
